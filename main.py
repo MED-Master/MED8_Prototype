a681a728254b996923a6bea2aa28c539f07c5e26
SteffenLehmann <slehma15@student.aau.dk>
2022-03-11 10:14:54 +0100

    Test

--- main.py
+++ main.py
@@ -10,7 +10,22 @@ from gtts import gTTS
 from playsound import playsound
 import os
 import pyttsx3
+import keyboard
+import pandas as pd
+import numpy
+#mircophone setup
+#print(s_r.__version__)
+#sr.Microphone.list_microphone_names()
+# print(s_r.Microphone.list_microphone_names()) #print all the microphones connected to your machine
+my_mic = sr.Microphone(device_index=1)
 
+#API setup
+WIT_AI_KEY = 'IQAXGNZQWIAZL5VAI7GUT6JK3P3RJ3TZ' #wit API key
+
+#data logging
+reply_logger = []
+
+#RASA setup
 engine = pyttsx3.init()
 bot_message = ""
 message=""
@@ -31,19 +46,30 @@ os.remove("welcome.mp3")
 #engine.say(bot_message)
 #engine.runAndWait()
 
-while bot_message != "Bye" or bot_message != 'thanks':
 
+
+while bot_message != "Bye" or bot_message != 'thanks':
     r = sr.Recognizer()  # initialize recognizer
-    with sr.Microphone() as source:  # mention source it will be either Microphone or audio files.
+    with my_mic as source:  # mention source it will be either Microphone or audio files.
         print("Speak Anything :")
+        r.adjust_for_ambient_noise(my_mic, duration=0.2)
         audio = r.listen(source)  # listen to the source
+
         try:
-            message = r.recognize_google(audio)  # use recognizer to convert our audio into text part.
+            message = r.recognize_wit(audio, key=WIT_AI_KEY) # use recognizer to convert our audio into text part.
+            reply_logger.append(message) #storing message string
             print("You said : {}".format(message))
 
-        except:
-            print("Sorry could not recognize your voice")  # In case of voice not recognized  clearly
-    if len(message)==0:
+        except sr.UnknownValueError:
+            print("I did not catch that, can you please say it again")
+            r = sr.Recognizer()
+            continue
+
+        except sr.RequestError as e:
+            print("Could not request results from API; {0}".format(e))
+            continue
+
+    if len(message) == 0:
         continue
     print("Sending message now...")
 
@@ -53,8 +79,14 @@ while bot_message != "Bye" or bot_message != 'thanks':
     for i in r.json():
         bot_message = i['text']
         print(f"{bot_message}")
+        reply_logger.append(bot_message)
         myobj = gTTS(text=bot_message)
         myobj.save("welcome.mp3")
         print('saved')
         playsound("welcome.mp3")
         os.remove("welcome.mp3")
+    if keyboard.is_pressed('l'):
+        dict = {'Replies': reply_logger}
+        df = pd.DataFrame(dict)
+        df.to_csv('Transcriptions.csv')
+        print("Logging complete")
