97102a122e27370eff7c15034fe1f75af8bd4d72
SteffenLehmann <slehma15@student.aau.dk>
2022-04-15 10:04:25 +0200

    Added plot file and updated the GUI design

--- GUI.py
+++ GUI.py
@@ -1,6 +1,29 @@
 from tkinter import *
 
+def round_rectangle(x1, y1, x2, y2, radius=25, **kwargs):
 
+    points = [x1+radius, y1,
+              x1+radius, y1,
+              x2-radius, y1,
+              x2-radius, y1,
+              x2, y1,
+              x2, y1+radius,
+              x2, y1+radius,
+              x2, y2-radius,
+              x2, y2-radius,
+              x2, y2,
+              x2-radius, y2,
+              x2-radius, y2,
+              x1+radius, y2,
+              x1+radius, y2,
+              x1, y2,
+              x1, y2-radius,
+              x1, y2-radius,
+              x1, y1+radius,
+              x1, y1+radius,
+              x1, y1]
+
+    return canvas.create_polygon(points, **kwargs, smooth=True)
 
 def btn_clicked():
     print("Button Clicked")
@@ -9,10 +32,10 @@ def btn_clicked():
 window = Tk()
 
 window.geometry("1699x900")
-window.configure(bg = "#a7a5a9")
+window.configure(bg = "#121212")
 canvas = Canvas(
     window,
-    bg = "#a7a5a9",
+    bg = "#121212",
     height = 900,
     width = 1699,
     bd = 0,
@@ -20,49 +43,12 @@ canvas = Canvas(
     relief = "ridge")
 canvas.place(x = 0, y = 0)
 
-
-HeaderShadow = canvas.create_rectangle(
-    10, 10, 10+1684, 10+47,
-    fill = "#666666",
-    outline = "")
-
-
-MainShadow = canvas.create_rectangle(
-    261, 69, 261+1433, 69+824,
-    fill = "#666666",
-    outline = "")
-
-
-MenuShadow = canvas.create_rectangle(
-    10, 69, 10+245, 69+824,
-    fill = "#666666",
-    outline = "")
-
-
-Header = canvas.create_rectangle(
-    10, 7, 10+1682, 7+48,
-    fill = "#e57bee",
-    outline = "")
-
-
-Menu = canvas.create_rectangle(
-    10, 66, 10+243, 66+825,
-    fill = "#818181",
-    outline = "")
-
-
-MainPlot = canvas.create_rectangle(
-    261, 66, 261+1431, 66+825,
-    fill = "#e3e1e1",
-    outline = "")
-
-DashboardName = canvas.create_text(
-    135.0, 27.5,
-    text = "Dashboard",
-    fill = "#eee9e9",
-    font = ("RobotoRoman-Regular", int(36.0)))
-
-print(MainShadow)
-
+canvas.pack()
+RoundRectangle = round_rectangle(25, 25, 25+247, 25+848, radius=20, fill="#272727")
+canvas.create_text(
+    148.5, 62.5,
+    text = "HISTORY",
+    fill = "#b7bbc5",
+    font = ("RobotoRoman-ExtraBold", int(24.0)))
 window.resizable(False, False)
 window.mainloop()
