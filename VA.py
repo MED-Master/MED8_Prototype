ebe0bc3e84d0aec1f48e6db921841c0a1daa57e7
SteffenLehmann <slehma15@student.aau.dk>
2022-04-16 11:06:59 +0200

    RASA works in GUI

---
+++ VA.py
@@ -0,0 +1,85 @@
+## Run this command in terminal  before executing this program
+## rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml
+## and also run this in seperate terminal
+## rasa run actions
+
+#Libraries
+import requests
+import speech_recognition as sr     # import the library
+import subprocess
+from gtts import gTTS
+from playsound import playsound
+import os
+import pyttsx3
+import keyboard
+import pandas as pd
+import numpy
+
+#Local Py files
+import Logging
+
+class RASA:
+    #mircophone setup
+    #print(s_r.__version__)
+    #sr.Microphone.list_microphone_names()
+    # print(s_r.Microphone.list_microphone_names()) #print all the microphones connected to your machine
+    my_mic = sr.Microphone(device_index=1)
+
+    #API setup
+    WIT_AI_KEY = 'IQAXGNZQWIAZL5VAI7GUT6JK3P3RJ3TZ' #wit API key
+    #RASA setup
+    engine = pyttsx3.init()
+    bot_message = ""
+    message=""
+    r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": "Hello"})
+
+
+    def VAIntro (self):
+        print("Bot says, ", end=' ')
+        for i in self.r.json():
+            self.bot_message = i['text']
+            print(f"{self.bot_message}")
+
+        myobj = gTTS(text=self.bot_message)
+        myobj.save("welcome.mp3")
+        print('saved')
+        playsound("welcome.mp3")
+        os.remove("welcome.mp3")
+        #VAIntro.engine.say(VAIntro.bot_message)
+        #VAIntro.engine.runAndWait()
+
+    def VATalkAndReply (self):
+        r = sr.Recognizer()  # initialize recognizer
+        with self.my_mic as source:  # mention source it will be either Microphone or audio files.
+            print("Speak Anything :")
+            r.adjust_for_ambient_noise(self.my_mic, duration=0.2)
+            audio = r.listen(source)  # listen to the source
+
+            try:
+                self.message = r.recognize_wit(audio, key=self.WIT_AI_KEY)  # use recognizer to convert our audio into text part.
+                Logging.reply_logger.append(self.message)  # storing message string
+                print("You said : {}".format(self.message))
+
+            except sr.UnknownValueError:
+                print("I did not catch that, can you please say it again")
+                r = sr.Recognizer()
+
+            except sr.RequestError as e:
+                print("Could not request results from API; {0}".format(e))
+
+        if len(self.message) == 0:
+            print("Sending message now...")
+
+        r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": self.message})
+
+        print("Bot says, ", end=' ')
+        for i in r.json():
+            self.bot_message = i['text']
+            print(f"{self.bot_message}")
+            Logging.reply_logger.append(self.bot_message)
+            myobj = gTTS(text=self.bot_message)
+            myobj.save("welcome.mp3")
+            print('saved')
+            playsound("welcome.mp3")
+            os.remove("welcome.mp3")
+
