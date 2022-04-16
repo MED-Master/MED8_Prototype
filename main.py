375e8e95bd13abcbd48bf4c8c0ff5dd69c877181
SteffenLehmann <slehma15@student.aau.dk>
2022-04-16 09:49:03 +0200

    Made RASA into a function

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import pyttsx3
 import keyboard
 import pandas as pd
 import numpy
+import Logging
 #mircophone setup
 #print(s_r.__version__)
 #sr.Microphone.list_microphone_names()
@@ -21,9 +22,6 @@ my_mic = sr.Microphone(device_index=1)
 
 #API setup
 WIT_AI_KEY = 'IQAXGNZQWIAZL5VAI7GUT6JK3P3RJ3TZ' #wit API key
-
-#data logging
-reply_logger = []
 
 #RASA setup
 engine = pyttsx3.init()
@@ -46,9 +44,7 @@ os.remove("welcome.mp3")
 #engine.say(bot_message)
 #engine.runAndWait()
 
-
-
-while True:
+def VirtualAgent ():
     r = sr.Recognizer()  # initialize recognizer
     with my_mic as source:  # mention source it will be either Microphone or audio files.
         print("Speak Anything :")
@@ -56,37 +52,32 @@ while True:
         audio = r.listen(source)  # listen to the source
 
         try:
-            message = r.recognize_wit(audio, key=WIT_AI_KEY) # use recognizer to convert our audio into text part.
-            reply_logger.append(message) #storing message string
+            message = r.recognize_wit(audio, key=WIT_AI_KEY)  # use recognizer to convert our audio into text part.
+            Logging.reply_logger.append(message)  # storing message string
             print("You said : {}".format(message))
 
         except sr.UnknownValueError:
             print("I did not catch that, can you please say it again")
             r = sr.Recognizer()
-            continue
 
         except sr.RequestError as e:
             print("Could not request results from API; {0}".format(e))
-            continue
 
     if len(message) == 0:
-        continue
-    print("Sending message now...")
+        print("Sending message now...")
 
     r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": message})
 
-    print("Bot says, ",end=' ')
+    print("Bot says, ", end=' ')
     for i in r.json():
         bot_message = i['text']
         print(f"{bot_message}")
-        reply_logger.append(bot_message)
+        Logging.reply_logger.append(bot_message)
         myobj = gTTS(text=bot_message)
         myobj.save("welcome.mp3")
         print('saved')
         playsound("welcome.mp3")
         os.remove("welcome.mp3")
 
-    dict = {'Replies': reply_logger}
-    df = pd.DataFrame(dict)
-    df.to_csv('Transcriptions.csv')
-    print("Logging complete")
+
+VirtualAgent()
